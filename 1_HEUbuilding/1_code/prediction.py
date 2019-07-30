ef09a0282860a52422d40b449f735063690e1601
Morgan Woods <freelightingwmh@gmail.com>
2019-07-30 12:41:40 +0800

    Add files via upload

---
+++ 1_HEUbuilding/1_code/prediction.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 29 16:46:17 2019
+
+@author: menghao
+"""
+
+# import the necessary packages
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+import numpy as np
+import imutils
+import pickle
+import cv2
+import os
+
+'''========================================
+            hyper-parameters
+   ========================================'''
+pic_name = '32'
+'''path'''
+root_path = os.getcwd() # get current working directory
+os.chdir('/Users/menghao/Document/2_project/1_HEUbuilding/result')
+
+img_path = '../3_newdata/'
+model_path = '../result/model.model'
+pickle_path = '../result/mlb.pickle'
+
+
+SCALE = (192, 108)
+
+# construct the argument parse and parse the arguments
+
+
+# load the image
+image = cv2.imread(img_path + pic_name + '.jpg')
+output = imutils.resize(image, width=400)
+ 
+# pre-process the image for classification
+image = cv2.resize(image, SCALE)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = image.astype("float") / 255.0
+image = img_to_array(image)
+image = np.expand_dims(image, axis=0)
+
+# load the trained convolutional neural network and the multi-label
+# binarizer
+print("[INFO] loading network...")
+model = load_model(model_path)
+mlb = pickle.loads(open(pickle_path, "rb").read())
+ 
+# classify the input image then find the indexes of the two class
+# labels with the *largest* probability
+print("[INFO] classifying image...")
+proba = model.predict(image)[0]
+idxs = np.argsort(proba)[::-1][:2]
+
+
+# loop over the indexes of the high confidence class labels
+for (i, j) in enumerate(idxs):
+	# build the label and draw the label on the image
+	label = "{}: {:.2f}%".format(mlb.classes_[j], proba[j] * 100)
+	cv2.putText(output, label, (10, (i * 30) + 25), 
+		cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+ 
+# show the probabilities for each of the individual labels
+for (label, p) in zip(mlb.classes_, proba):
+	print("{}: {:.2f}%".format(label, p * 100))
+ 
+# show the output image
+cv2.imshow("Output", output)
+cv2.waitKey(10000)
+
+
+
+
+
